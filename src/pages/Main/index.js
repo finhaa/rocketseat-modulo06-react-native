f8d764b96c1e5af6c27b89160d497e9113f774fa
Finha <finha@MacBook-Air-de-Finha.local>
2020-05-04 18:12:46 -0300

    Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const Main = () => {
-  return <View />;
+  return <Container />;
 };
 
 Main.navigationOptions = {
