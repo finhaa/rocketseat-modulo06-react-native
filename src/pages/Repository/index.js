935b584fa78cbd1614b2820894dfa971cc8da694
Rafael Farias <finha.farias@outlook.com>
2020-05-11 17:28:26 -0300

    Tentativa de centralizar loading spinner

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import PropTypes from 'prop-types';
 
-import { Browser } from './styles';
+import { Container, Browser } from './styles';
 
-export default function Repository({ navigation }) {
-  const repository = navigation.getParam('repository');
+export default class Repository extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
 
-  return <Browser source={{ uri: repository.html_url }} />;
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('repository').name,
+  });
+
+  state = {
+    loading: true,
+  };
+
+  load() {
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const repository = navigation.getParam('repository');
+
+    const { loading } = this.state;
+
+    return (
+      <Container>
+        <Browser
+          onLoad={() => this.load()}
+          source={{ uri: repository.html_url }}
+        />
+        {loading && <ActivityIndicator size="large" color="#7159c1" />}
+      </Container>
+    );
+  }
 }
-
-Repository.propTypes = {
-  navigation: PropTypes.shape({
-    getParam: PropTypes.func,
-  }).isRequired,
-};
-
-Repository.navigationOptions = ({ navigation }) => ({
-  title: navigation.getParam('repository').name,
-});
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -1,6 +1,14 @@
 import styled from 'styled-components/native';
 import { WebView } from 'react-native-webview';
 
+export const Container = styled.View`
+  flex: 1;
+`;
+
 export const Browser = styled(WebView)`
   flex: 1;
+`;
+
+export const LoadingSpinner = styled.ActivityIndicator`
+  position: absolute;
 `;
